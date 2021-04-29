86f9af968f24a768d5f259f9860c61887efae61f
avril-m-harder <avrilharder@gmail.com>
2021-04-29 18:22:45 -0500

    update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # krat_remote_sensing_scripts
+## Data download
+	1. index.csv: contains information on scenes available for download from the Google Cloud Platform (downloaded on 4/28/21)
+	2. scene_dl_select.R: reads in index.csv and writes a list of scenes to be downloaded (scenes_to_dl.txt) using gsutils
+	3. gsutils_download: downloads Landsat scene data 
--- gsutils_download.sh
+++ gsutils_download.sh
@@ -2,9 +2,9 @@
 ## of scenes of interest (n=648 in this case: all Landsat 4-5TM scenes overlapping GPS
 ## coords of mounds, 31.6166667°, -109.2500000°)
 
-./gsutils/google-cloud-sdk/bin/gcloud init
+# ./gsutils/google-cloud-sdk/bin/gcloud init
 
-cat 648_gsutil_URLs.txt | while read line
+cat scenes_to_dl.txt | while read line
  	do
  	gsutil cp -r "${line}" ./ls_downloads/
  	done
