23b9419763a491a5cabef2a44d8fe08e2800457a
avril-m-harder <avrilharder@gmail.com>
2021-05-10 10:06:21 -0500

    old and new comps

--- landsat_processing_C2.R
+++ landsat_processing_C2.R
@@ -9,6 +9,7 @@ library(ggplot2)
 library(scales)
 options("rgdal_show_exportToProj4_warnings"="none")
 library(rgdal)
+library(TeachingDemos)
 source('/Users/Avril/Documents/krat_remote_sensing/krat_remote_sensing_scripts/c2l2readMeta.R')
 `%notin%` <- Negate(`%in%`)
 ##### 1. Read in mound waypoints for later extraction #####
@@ -33,7 +34,7 @@ mnd.locs <- sp::spTransform(mnd.locs, CRS("+proj=utm +zone=12 +ellps=WGS84 +towg
 # hi.y <- 3499750
 # ext <- extent(lo.x, hi.x, lo.y, hi.y)
 # 
-# for(i in 58:length(dirs)){
+# for(i in 1:length(dirs)){
 #   setwd(paste0('/Volumes/avril_data/krat_remote_sensing/C2L2_raw_landsat45tm_scene_downloads/',dirs[i],'/'))
 #   ## read in data
 #   all_landsat_bands <- list.files(pattern = glob2rx("*TIF$"), full.names = TRUE) ## get all bands, including QA and CLOUD_QA
@@ -68,8 +69,8 @@ mnd.locs <- sp::spTransform(mnd.locs, CRS("+proj=utm +zone=12 +ellps=WGS84 +towg
 
 ##### 3. Loop over cropped scenes, process, and write TC output #####
 ## set file names for run
-tc.fn <- 'tc_initial_test' ## for TC data
-mc.fn <- 'mnd_key_initial_test' ## for mound/cell ID key
+tc.fn <- 'tc_cloud_free' ## for TC data -- tc_initial_test    tc_cloud_free
+mc.fn <- 'mnd_key_cloud_free' ## for mound/cell ID key -- mnd_key_initial_test    mnd_key_cloud_free
 
 ## set extent variables
 lo.x <- 663500
@@ -80,11 +81,12 @@ hi.y <- 3499750
 # setwd('/Volumes/avril_data/krat_remote_sensing/cropped_landsat45tm_scenes/')
 setwd('/Users/Avril/Documents/krat_remote_sensing/C2L2_cropped_landsat45tm_scenes/')
 # files <- list.files(pattern="*.grd") ## to run with ALL scenes (causes some issues due to cloud masking step)
-files <- read.table('/Users/Avril/Documents/krat_remote_sensing/C2L2_landsat_5_data_overviews/C2L2_intermediate_cloud_scenes.txt') ## scenes with <=60% cloud cover (n=574)
+# files <- read.table('/Users/Avril/Documents/krat_remote_sensing/C2L2_landsat_5_data_overviews/C2L2_intermediate_cloud_scenes.txt') ## scenes with <=60% cloud cover (n=574)
+files <- read.table('/Users/Avril/Documents/krat_remote_sensing/C2L2_landsat_5_data_overviews/C2L2_cloud_free_scenes.txt') ## scenes with 0% cloud cover (n=233)
 files$V1 <- paste0(files$V1,'_CROPPED.grd')
 files <- as.vector(files$V1)
 
-pdf(file='/Users/Avril/Desktop/test.pdf', width=8, height=8)
+pdf(file='/Users/Avril/Desktop/test_cloud_free.pdf', width=8, height=8)
 par(mar=c(3.1,2.1,2.1,1.1), mgp=c(1.5,.75,0))
 
 OUT <- NULL
@@ -143,6 +145,7 @@ for(i in 1:length(files)){
 
   ## define data to be saved and format for writing output
   scene.id <- m.data$SCENE_ID
+  prod.id <- gsub('_CROPPED.grd', '', files[i])
   acq.date <- m.data$ACQUISITION_DATE
   path <- m.data$PATH_ROW[1]
   row <- m.data$PATH_ROW[2]
@@ -152,6 +155,7 @@ for(i in 1:length(files)){
   all.tc$acq.date <- acq.date
   all.tc$doy <- doy
   all.tc$scene.id <- scene.id
+  all.tc$prod.id <- prod.id
   all.tc$path <- path
   all.tc$row <- row
   all.tc$nrows <- ls5.tc.cor$brightness@nrows
@@ -167,6 +171,7 @@ for(i in 1:length(files)){
   mnd.cell.dat$acq.date <- acq.date
   mnd.cell.dat$doy <- doy
   mnd.cell.dat$scene.id <- scene.id
+  mnd.cell.dat$prod.id <- prod.id
   mnd.cell.dat$path <- path
   mnd.cell.dat$row <- row
 
@@ -180,12 +185,39 @@ dev.off()
 
 ##### *3. Need to manually review test.pdf for cloud cover #####
 ## (empty plots represent bricks that could not be plotted due to plotRGB scaling issue) ##
+## clouds in: 
+## 24 − LT05_L2SP_035038_19890906_20200916_02_T1_CROPPED.grd
+## 32 − LT05_L2SP_035038_19900402_20200916_02_T1_CROPPED.grd
+## 49 − LT05_L2SP_035038_19910320_20200915_02_T1_CROPPED.grd
+## 58 − LT05_L2SP_034038_19910601_20200915_02_T1_CROPPED.grd
+## (?) 59 − LT05_L2SP_035038_19910608_20200915_02_T1_CROPPED.grd
+## 63 − LT05_L2SP_034038_19910703_20200915_02_T1_CROPPED.grd
+## 69 − LT04_L2SP_035038_19910904_20200915_02_T1_CROPPED.grd
+## (?) 73 − LT05_L2SP_034038_19911023_20200915_02_T1_CROPPED.grd
+## (?) 79 − LT05_L2SP_034038_19920502_20200915_02_T2_CROPPED.grd
+## 86 − LT05_L2SP_034038_19920619_20200914_02_T1_CROPPED.grd
+## 92 − LT05_L2SP_034038_19920806_20200914_02_T1_CROPPED.grd
+## 94 − LT05_L2SP_035038_19920829_20200914_02_T2_CROPPED.grd
+## 109 − LT05_L2SP_035038_19930120_20200914_02_T1_CROPPED.grd
+## 120 − LT05_L2SP_035038_19930426_20200914_02_T1_CROPPED.grd
+## 123 − LT05_L2SP_034038_19930606_20200914_02_T1_CROPPED.grd
+## (?) 128 − LT05_L2SP_034038_19930724_20200913_02_T1_CROPPED.grd [stopped here for now]
+
+## for cloud-free scenes, corrupted info in:
+## 90, 92, 93, 103, 105, 107, 109, 110, 112, 113, 116, 119, 122, 156, 159, 166
+## 156, 159, 166 really weird - like they're not the correct location?
+remove <- c(90, 92, 93, 103, 105, 107, 109, 110, 112, 113, 116, 119, 122, 156, 159, 166)
+scenes.rm <- gsub('_CROPPED.grd', '', files[remove])
 
 ##### Do some data viz for current run #####
-setwd('/Volumes/avril_data/krat_remote_sensing/tc_output_tables/')
+setwd('/Users/Avril/Documents/krat_remote_sensing/C2L2_tc_output_tables/')
 
-tc.dat <- read.csv(paste0(tc.fn,'.csv'))
-mc.key <- read.csv(paste0(mc.fn,'.csv'))
+tc.dat <- read.csv(paste0('C2L2_',tc.fn,'.csv'))
+mc.key <- read.csv(paste0('C2L2_',mc.fn,'.csv'))
+
+## remove scenes with weird data (for cloud-free scenes, this really improves the outlier situation)
+tc.dat <- tc.dat[which(tc.dat$prod.id %notin% scenes.rm),]
+mc.key <- mc.key[which(mc.key$prod.id %notin% scenes.rm),]
 
 ## for a single cell, plot trends in TC metrics over year
 temp <- tc.dat[which(tc.dat$cells == unique(tc.dat$cells)[1]),]
@@ -209,6 +241,7 @@ plot(temp$doy, temp$brightness, pch=19, col=temp$year, cex=0.8)
 plot(temp1$doy, temp1$brightness, pch=19, col=temp$year, cex=0.8)
 
 ## plot lines by year within a cell
+par(mfrow=c(1,1))
 palette.pals()
 cols <- palette.colors(n=length(unique(temp$year)), palette='Polychrome36', recycle=FALSE)
 plot(temp$doy, temp$greenness, col='transparent')
@@ -219,5 +252,54 @@ for(i in 1:length(unique(temp$year))){
   lines(x=sub$doy, y=sub$greenness, col=cols[i])
 }
 
-
-
+## compare values with old C1 TC values
+old.dat.tc <- read.csv('/Users/Avril/Documents/krat_remote_sensing/archive/all_C1L1_data_and_output/C1L1_tc_output_tables/tc_cloud_free.csv')
+old.dat.tc <- old.dat.tc[which(old.dat.tc$scene.id %in% tc.dat$scene.id),]
+pdf('/Users/Avril/Desktop/C1L1_v_C2L2_greenness.pdf', width=7, height=7)
+plot(old.dat.tc$greenness, tc.dat$greenness, col='transparent', xlab='C1L1 greenness', ylab='C2L2 greenness')
+for(i in unique(old.dat.tc$scene.id)){
+  old.sub <- old.dat.tc[which(old.dat.tc$scene.id == i),]
+  new.sub <- tc.dat[which(tc.dat$scene.id == i),]
+  combo <- merge(x=old.sub, y=new.sub, by='cells')
+  mod <- lm(combo$greenness.y ~ combo$greenness.x)
+  min.x <- min(combo$greenness.x)
+  max.x <- max(combo$greenness.x)
+  min.y <- ((mod$coefficients[2])*min.x)+mod$coefficients[1]
+  max.y <- ((mod$coefficients[2])*max.x)+mod$coefficients[1]
+  lines(x=c(min.x, max.x), y=c(min.y, max.y), col=alpha('springgreen4', 0.4))
+  # points(combo$greenness.x, combo$greenness.y, pch=19, col=alpha('springgreen4', 0.01))
+  print(i)
+}
+dev.off()
+pdf('/Users/Avril/Desktop/C1L1_v_C2L2_wetness.pdf', width=7, height=7)
+plot(old.dat.tc$wetness, tc.dat$wetness, col='transparent', xlab='C1L1 wetness', ylab='C2L2 wetness')
+for(i in unique(old.dat.tc$scene.id)){
+  old.sub <- old.dat.tc[which(old.dat.tc$scene.id == i),]
+  new.sub <- tc.dat[which(tc.dat$scene.id == i),]
+  combo <- merge(x=old.sub, y=new.sub, by='cells')
+  mod <- lm(combo$wetness.y ~ combo$wetness.x)
+  min.x <- min(combo$wetness.x)
+  max.x <- max(combo$wetness.x)
+  min.y <- ((mod$coefficients[2])*min.x)+mod$coefficients[1]
+  max.y <- ((mod$coefficients[2])*max.x)+mod$coefficients[1]
+  lines(x=c(min.x, max.x), y=c(min.y, max.y), col=alpha('dodgerblue', 0.4))
+  # points(combo$wetness.x, combo$wetness.y, pch=19, col=alpha('dodgerblue', 0.01))
+  print(i)
+}
+dev.off()
+pdf('/Users/Avril/Desktop/C1L1_v_C2L2_brightness.pdf', width=7, height=7)
+plot(old.dat.tc$brightness, tc.dat$brightness, col='transparent', xlab='C1L1 brightness', ylab='C2L2 brightness')
+for(i in unique(old.dat.tc$scene.id)){
+  old.sub <- old.dat.tc[which(old.dat.tc$scene.id == i),]
+  new.sub <- tc.dat[which(tc.dat$scene.id == i),]
+  combo <- merge(x=old.sub, y=new.sub, by='cells')
+  mod <- lm(combo$brightness.y ~ combo$brightness.x)
+  min.x <- min(combo$brightness.x)
+  max.x <- max(combo$brightness.x)
+  min.y <- ((mod$coefficients[2])*min.x)+mod$coefficients[1]
+  max.y <- ((mod$coefficients[2])*max.x)+mod$coefficients[1]
+  lines(x=c(min.x, max.x), y=c(min.y, max.y), col=alpha('tan4', 0.4))
+  # points(combo$brightness.x, combo$brightness.y, pch=19, col=alpha('tan4', .01))
+  print(i)
+}
+dev.off()
